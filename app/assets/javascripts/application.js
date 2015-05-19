42bc0b5737a9401ca5b58632bac8ea65a2e0d5a1
khanhkhanh <ngo.duy.khanh@framgia.com>
2015-05-19 16:27:28 +0700

    Ajaxify CRUD User functions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,14 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+$(document).ajaxError(function(event, xhr, options, exc) {
+  var errors = JSON.parse(xhr.responseText);
+  var er = "<ul>";
+  for(var i = 0; i < errors.length; i++) {
+    var list = errors[i];
+    er += "<li>" + list + "</li>"
+  }
+  er += "</ul>"
+  $("#error_explanation").html(er);
+});
--- app/controllers/supervisor/users_controller.rb
+++ app/controllers/supervisor/users_controller.rb
@@ -1,37 +1,59 @@
 class Supervisor::UsersController < ApplicationController
+  before_action :set_user, except: [:index, :new, :create]
+
   def index
     @users = User.paginate page: params[:page], per_page: 10
+  end
+
+  def show
   end
 
   def new
     @user = User.new
   end
 
-  def edit
-    @user = User.find params[:id]
-  end
-
-  def show
-    @user = User.find params[:id]
-  end
-
   def create
     @user = User.new user_params
-    if @user.save
-      flash[:success] = t(:user_created)
-      redirect_to supervisor_users_path
-    else
-      render 'new'
+    respond_to do |format|
+      if @user.save
+        format.json {head :no_content}
+        format.js
+      else
+        format.json {render json: @user.errors.full_messages, 
+                            status: :unprocessable_entity}
+      end
+    end
+  end
+ 
+  def edit
+  end
+ 
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.json {head :no_content}
+        format.js
+      else
+        format.json {render json: @user.errors.full_messages,
+                                   status: :unprocessable_entity}
+      end
     end
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = t(:user_deleted)
-    redirect_to supervisor_users_path
+    @user.destroy!
+    respond_to do |format|
+      format.js
+      format.html {redirect_to users_url}
+      format.json {head :no_content}
+    end
   end
-  
+
   private
+  def set_user
+    @user = User.find params[:id]
+  end
+
   def user_params
     params.require(:user).permit :name, :email, :password,
                                  :password_confirmation
